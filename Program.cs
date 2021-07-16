37ee0facde5295373b56e357a4e20e3866d322f2
karthik413 <rago.karthik2@gmail.com>
2021-07-16 15:22:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverseprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter String Input");
+            string stringInput = Console.ReadLine();
+            Console.WriteLine(stringInput.Length == 0 ? "Please enter valid input" : Program.reverseMethod(stringInput));
+            Console.ReadLine();
+        }
+
+        public static string reverseMethod(string stringInput)
+        {
+            try
+            {
+                char[] stringArray = stringInput.ToCharArray();
+
+                string reverse = String.Empty;
+
+                for (int i = stringArray.Length - 1; i >= 0; i--)
+                {
+                    reverse += stringArray[i];
+                }
+
+             
+                return reverse;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
